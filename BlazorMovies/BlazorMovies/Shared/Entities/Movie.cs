3a84513062204e026b3572434400a3ea8ab7c325
WarOfDevil <vnktux@protonmail.com>
2020-08-17 22:10:42 +0200

    Add Movie entity

---
+++ BlazorMovies/BlazorMovies/Shared/Entities/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
