026b8032899d2939a183c7b1d30fec80898b926b
WarOfDevil <vnktux@protonmail.com>
2020-09-06 17:18:16 +0200

    Completed edit persona component

---
+++ BlazorMovies/BlazorMovies/Client/Shared/Components/InputMarkdown.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq.Expressions;
+
+namespace BlazorMovies.Client.Shared.Components
+{
+    public partial class InputMarkdown<TValue>
+    {
+        [Parameter]
+        public Expression<Func<TValue>> For { get; set; }
+
+        [Parameter]
+        public string Label { get; set; }
+    }
+}
